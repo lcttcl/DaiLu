88ef1810fe950ad3756f1a3a2700f5a6ea4ee940
lct-windows <lichangtai19@163.com>
2020-07-09 00:07:18 +0800

    大洲、国家、媒体-1

--- apps/mediums/views.py
+++ apps/mediums/views.py
@@ -9,10 +9,10 @@ import csv
 # Create your views here.
 
 def media_list(request, country_pk=0):
+    media = MediumInfo.objects.filter(is_deleted=False)
     if country_pk != 0:
-        media = MediumInfo.objects.filter(is_deleted=False, country_info_id=country_pk)
-    else:
-        media = MediumInfo.objects.filter(is_deleted=False)
+        media = media.filter(country_info_id=country_pk)
+
     countries = CountryInfo.objects.all()
     context = {
         'media': media,
@@ -27,20 +27,34 @@ def country_list(request):
 
 def world_map(request):
     countries = CountryInfo.objects.all()
-    # 分页功能
-    page_num = request.GET.get('page_num')
-    pa = Paginator(countries, 2)
-    try:
-        pages = pa.page(page_num)
-    except PageNotAnInteger:
-        pages = pa.page(1)
-    except EmptyPage:
-        pages = pa.page(pa.num_pages)
+    media = MediumInfo.objects.filter(is_deleted=False)
 
+    continent = request.GET.get('continent', '')
+    if continent:
+        countries = countries.filter(continent=continent)
+        media = MediumInfo.objects.filter(country_info__continent=continent)
+
+    countries = countries.order_by("-name")
+
+    country_id = request.GET.get('country_id', '')
+    if country_id:
+        media = MediumInfo.objects.filter(country_info_id=int(country_id))
+        # 分页功能
+        # page_num = request.GET.get('page_num','')
+        # pa = Paginator(countries, 10)
+        # try:
+        #     pages = pa.page(page_num)
+        # except PageNotAnInteger:
+        #     pages = pa.page(1)
+        # except EmptyPage:
+        #     pages = pa.page(pa.num_pages)
     context = {
         'countries': countries,
-        'pages': pages,
-        'pa': pa,
+        # 'pages': pages,
+        # 'pa': pa,
+        'continent': continent,
+        'country_id': country_id,
+        'media': media,
     }
     return render(request, 'world_map.html', context)
 
